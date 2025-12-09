6fc94c07ad8cdbd9c7973f5ae445f30613b120d9
G-Zale <zalepugag@gmail.com>
2025-12-09 16:39:25 +0200

    uzduotis C 1

--- G-zale/app.js
+++ G-zale/app.js
@@ -1,1 +1,15 @@
-console.log('labas');
+//uzduotis C
+
+  (function () {
+    const btnX = document.querySelector('.btnX');
+    const btnO = document.querySelector('.btnO');
+
+    function swapTexts() {
+      const temp = btnX.innerText;
+      btnX.innerText = btnO.innerText;
+      btnO.innerText = temp;
+    }
+
+    btnX.addEventListener('click', swapTexts);
+    btnO.addEventListener('click', swapTexts);
+  })();
